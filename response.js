29ea494a39280ea4f3c204bb0875ec51ac1b96f4
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-02-17 14:30:08 +0900

    set timer for click, but error is found!

--- response.js
+++ response.js
@@ -2,20 +2,34 @@
 
 const screen=document.querySelector("#screen");
 
+
 screen.addEventListener('click',function(){
+    let startTime;
+
 
     if(screen.classList.contains('waiting')){
 
         screen.classList.remove('waiting');
         screen.classList.add('ready');
         screen.textContent='If it is green, then click!';
+        
+        setTimeout(function(){
+            startTime=new Date();
+            screen.click();
+        },Math.floor(Math.random()*1000)+2000);
+
     }
     else if(screen.classList.contains('ready')){
+      
 
         screen.classList.remove('ready');
         screen.classList.add('now');
         screen.textContent='now click!!!';
+      
     }else if(screen.classList.contains('now')){
+
+        let endTime=new Date();
+        console.log('your response time is : '(endTime-startTime)/1000,'ms');
 
         screen.classList.remove('now');
         screen.classList.add('waiting');
