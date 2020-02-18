7c1894848c22ae6a7b1702d5b9a91838a8550146
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-02-18 16:17:25 +0900

    cheating detection added

--- response.js
+++ response.js
@@ -2,7 +2,9 @@
 
 const screen=document.querySelector("#screen");
 
-let startTime,endTime;//variable gloabalizing 
+let startTime,endTime,timeOut;//variable gloabalizing 
+
+let history=[];
 
 screen.addEventListener('click',function(){
    
@@ -14,24 +16,34 @@ screen.addEventListener('click',function(){
         screen.classList.add('ready');
         screen.textContent='If it is green, then click!';
         
-        setTimeout(function(){
+         timeOut=setTimeout(function(){
             startTime=Date.now();
             screen.click();
         },Math.floor(Math.random()*1000)+2000);
 
     }
     else if(screen.classList.contains('ready')){
-      
-
+      if(!startTime){//if cheating happen,
+          clearTimeout(timeOut);
+      }
+      else{
         screen.classList.remove('ready');
         screen.classList.add('now');
-        screen.textContent='now click!!!';
+        screen.textContent='now click!!!'; 
+      }
+
+        
       
     }else if(screen.classList.contains('now')){
 
 
         endTime=Date.now();
         console.log('your response time is : ',(endTime-startTime)/1000,'ms');//<-',' added
+
+        history.push((endTime-startTime)/1000);
+
+        startTime=null;//initializing
+        endTime=null;//initializing
 
         screen.classList.remove('now');
         screen.classList.add('waiting');
